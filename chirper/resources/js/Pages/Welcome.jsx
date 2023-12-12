4c34f194c262f6af4c7ff5d8c0b6a395242a2aec
przemyslawkrepski <krepski80@gmail.com>
2023-12-12 12:07:43 +0100

    first welcome style

--- chirper/resources/js/Pages/Welcome.jsx
+++ chirper/resources/js/Pages/Welcome.jsx
@@ -1,6 +1,8 @@
 import {Link, Head} from '@inertiajs/react';
+import mealImage from '/public/images/assets/meal.jpg'
 
 export default function Welcome({auth, laravelVersion, phpVersion}) {
+    // const bannerImageUrl = "https://source.unsplash.com/jUPOXXRNdcA";
     return (
         <>
             <Head title="Welcome"/>
@@ -33,18 +35,16 @@ export default function Welcome({auth, laravelVersion, phpVersion}) {
                     )}
                 </div>
 
-                <div className="max-w-7xl min-w-5xl mx-auto p-6 lg:p-8 bg-white">
-
-
-                    <section className='header'>
-                        <div className="banner">
-                            <h1 className="hero">DishDock</h1>
-                            <p className="hero_description">tutaj znajdziesz wszystko co potrzebne</p>
-                            <button className="login_button">Zaloguj się</button>
+                <div className="main w-4/5 mx-auto" style={{ height: '80vh' }}>
+                    <section className='header h-1/2'>
+                        <div className="main h-full mx-auto ">
+                                <div className="banner relative h-full bg-cover bg-center" style={{ backgroundImage: `url(/images/assets/meal.jpg)`  }}>
+                                    <button className="bg-blue-300 w-32 h-16 p-2 text-white rounded login_button absolute inset-0 m-auto">Zaloguj się</button>
+                                </div>
                         </div>
                     </section>
-                    <section>
-                        <div className="wrapper">
+                    <section className='h-1/2'>
+                        <div className="wrapper flex justify-around">
                             <div className="card">
                                 <h4 className="card_title">Znajdz Przepis</h4>
                                 <img src="" alt="" className="card_img"/>
@@ -67,16 +67,16 @@ export default function Welcome({auth, laravelVersion, phpVersion}) {
                 </div>
             </div>
 
-            <style>{`
-                .bg-dots-darker {
-                    background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
-                }
-                @media (prefers-color-scheme: dark) {
-                    .dark\\:bg-dots-lighter {
-                        background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
-                    }
-                }
-            `}</style>
+            {/*<style>{`*/}
+            {/*    .bg-dots-darker {*/}
+            {/*        background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");*/}
+            {/*    }*/}
+            {/*    @media (prefers-color-scheme: dark) {*/}
+            {/*        .dark\\:bg-dots-lighter {*/}
+            {/*            background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");*/}
+            {/*        }*/}
+            {/*    }*/}
+            {/*`}</style>*/}
         </>
     );
 }
